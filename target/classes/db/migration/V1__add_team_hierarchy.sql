fde499a6f8a2df98be7fc502ca5db03b523d4905
Saurabh-2151 <saurabhganjale.sits.comp@gmail.com>
2026-01-05 13:33:48 +0530

    New Updates

--- target/classes/db/migration/V1__add_team_hierarchy.sql
+++
@@ -1,9 +0,0 @@
--- Add parent_team_id column to teams table
-ALTER TABLE teams ADD COLUMN parent_team_id BIGINT;
-
--- Add foreign key constraint for parent team
-ALTER TABLE teams ADD CONSTRAINT fk_team_parent_team 
-FOREIGN KEY (parent_team_id) REFERENCES teams(id);
-
--- Add index for better performance
-CREATE INDEX idx_team_parent_team_id ON teams(parent_team_id);
